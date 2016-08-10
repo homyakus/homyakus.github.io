161ea9bc5bdc5eadbf192bd2d80913fdf8d33b3f
Никита <homyakus2009@gmail.com>
2016-08-10 18:29:52 +0700

    10.08.2016#3

--- MN/js/common.js
+++ MN/js/common.js
@@ -40,11 +40,11 @@ $(function() {
 		// opens .a of selected question
 		$(this).next().slideToggle(speed)    
 		// selects all other answers and slides up any open answer
-		.siblings('li.a').slideUp();  
+		// .siblings('li.a').slideUp();  
 		// Grab img from clicked question
 		var img = $(this).children('img');
 		// remove Rotate class from all images except the active
-		$('img').not(img).removeClass('rotate');
+		// $('img').not(img).removeClass('rotate');
 		// toggle rotate class
 		img.toggleClass('rotate');
 	});
