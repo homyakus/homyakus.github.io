9f6a0a0558d821c28c0b5f1cd3b10abcf3c594fa
Никита <homyakus2009@gmail.com>
2016-08-10 18:24:04 +0700

    10.08.2016#2

--- MN/js/common.js
+++ MN/js/common.js
@@ -30,4 +30,22 @@ $(function() {
 		loop: true,
 		navText: ["<i class='icon-left'></i>", "<i class='icon-right'></i>"]
 	});
+
+
+	var action = 'click';
+	var speed = "500";
+	// Question handler
+	$('li.q').on(action, function(){
+		// gets next element
+		// opens .a of selected question
+		$(this).next().slideToggle(speed)    
+		// selects all other answers and slides up any open answer
+		.siblings('li.a').slideUp();  
+		// Grab img from clicked question
+		var img = $(this).children('img');
+		// remove Rotate class from all images except the active
+		$('img').not(img).removeClass('rotate');
+		// toggle rotate class
+		img.toggleClass('rotate');
+	});
 });
